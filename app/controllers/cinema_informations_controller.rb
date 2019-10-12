e6cbb7f8432053f21fe90ce259fc0c062d39e732
t-mangoe <m.takashi.xpair@gmail.com>
2019-10-13 00:42:25 +0900

    nokogiriでのパース処理を新規作成（未完成だが）

--- app/controllers/cinema_informations_controller.rb
+++ app/controllers/cinema_informations_controller.rb
@@ -1,4 +1,21 @@
+require "open-uri"
+require "kconv"
+# require "nokogiri"
+
 class CinemaInformationsController < ApplicationController
   def home
+    url = "https://www.jollios.net/cgi-bin/pc/site/det.cgi?tsc=21080"
+
+
+    # バイナリで読み込んで、文字化けを回避
+    html = open(url, 'r:binary').read
+    doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
+    p doc.title
+    all = doc.xpath("//*")
+    # p all
+    # p doc.xpath("//div[@id='wrapper']")
+    @movies = doc.xpath("//div[@class='tTableSiteDet']")
+    # @movies = doc.xpath("//div[@class='tTableSiteDet']")
+    # p @movies
   end
 end
