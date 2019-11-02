01f79cd894a7287b60aeedc20592cff5d18a4e65
t-mangoe <m.takashi.xpair@gmail.com>
2019-11-02 16:03:41 +0900

    不要なコメントを削除

--- app/controllers/cinema_informations_controller.rb
+++ app/controllers/cinema_informations_controller.rb
@@ -33,39 +33,6 @@ class CinemaInformationsController < ApplicationController
       end
       @infos << info
     end
-
-    # url = "https://www.jollios.net/cgi-bin/pc/site/det.cgi?tsc=21080"
-
-    # # バイナリで読み込んで、文字化けを回避
-    # html = open(url, "r:binary").read
-    # doc = Nokogiri::HTML.parse(html.toutf8, nil, "utf-8")
-
-    # @movies = doc.xpath("//div[@class='tTableSiteDet']")
-    # @infos = []
-    # @movies.each do |movie|
-    #   info = CinemaInfo.new
-
-    #   # 映画のタイトルの取り出し
-    #   title = movie.xpath(".//div[@class='title']")
-    #   title = title.xpath(".//a")
-    #   info.title = title.text
-
-    #   # 映画の開始時間の取り出し
-    #   time_schedules = movie.xpath(".//div[contains(@id, 'sTime')]")
-    #   time_schedules.each do |schedule|
-    #     # モデルのオブジェクトを作成
-    #     cinema_showing_record = CinemaShowing.new
-    #     cinema_showing_record.title = info.title
-    #     cinema_showing_record.date = Date.today.strftime("%Y-%m-%d")
-
-    #     start_time = schedule.xpath(".//div[@class='start']").text
-    #     info.add_start_time(start_time)
-    #     cinema_showing_record.start = start_time
-    #     cinema_showing_record.save
-    #   end
-
-    #   @infos << info
-    # end
   end
 
   def create_showing_record
@@ -77,14 +44,11 @@ class CinemaInformationsController < ApplicationController
     doc = Nokogiri::HTML.parse(html.toutf8, nil, "utf-8")
 
     movies = doc.xpath("//div[@class='tTableSiteDet']")
-    # @infos = []
     movies.each do |movie|
-      # info = CinemaInfo.new
 
       # 映画のタイトルの取り出し
       title = movie.xpath(".//div[@class='title']")
       title = title.xpath(".//a")
-      # info.title = title.text
 
       # 映画の開始時間の取り出し
       time_schedules = movie.xpath(".//div[contains(@id, 'sTime')]")
@@ -95,12 +59,9 @@ class CinemaInformationsController < ApplicationController
         cinema_showing_record.date = Date.today.strftime("%Y-%m-%d")
 
         start_time = schedule.xpath(".//div[@class='start']").text
-        # info.add_start_time(start_time)
         cinema_showing_record.start = start_time
         cinema_showing_record.save
       end
-
-      # @infos << info
     end
   end
 
